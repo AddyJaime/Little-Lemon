7111a100ccf86ca67bf7d05467e444c827fda781
addy <addy@gmail.com>
2025-01-06 09:58:54 -0500

    little lemon files and folder set up

---
+++ src/components/LittleLemonHeader.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import { View, Text } from "react-native";
+
+const LittleLemonHeader: React.FC = () => {
+  return <View></View>;
+};
+
+export default LittleLemonHeader;
