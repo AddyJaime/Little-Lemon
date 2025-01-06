f32543e699725c12ff9576bcddb8eec31ed0d22d
addy <addy@gmail.com>
2025-01-06 10:13:26 -0500

    little lemon header was created

--- src/components/LittleLemonHeader.tsx
+++ src/components/LittleLemonHeader.tsx
@@ -1,8 +1,30 @@
 import React from "react";
-import { View, Text } from "react-native";
+import { View, Text, StyleProp, StyleSheet } from "react-native";
 
 const LittleLemonHeader: React.FC = () => {
-  return <View></View>;
+  return (
+    <View style={styles.container}>
+      <Text style={styles.h1} numberOfLines={3}>
+        Welcome
+      </Text>
+      <Text style={styles.title}>Little Lemon</Text>
+    </View>
+  );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 0.2,
+    backgroundColor: "#F4CE14",
+  },
+  h1: {
+    padding: 40,
+    fontSize: 30,
+    color: "black",
+  },
+  title: {
+    fontWeight: "bold",
+  },
+});
 
 export default LittleLemonHeader;
