9318d7f29dfef834771b0aefd08fbf3ce3c8b8b7
addy <addy@gmail.com>
2025-01-07 11:39:00 -0500

    scroll view in processs

--- src/components/WelcomeScreen.tsx
+++ src/components/WelcomeScreen.tsx
@@ -1,15 +1,17 @@
 import React from "react";
-import { View, Text, StyleSheet } from "react-native";
+import { View, Text, StyleSheet, ScrollView } from "react-native";
 
 const WelcomeScreen: React.FC = () => {
   return (
     <View style={styles.container}>
-      <Text style={styles.h1}>Welcome to Little Lemon</Text>
-      <Text style={styles.p}>
-        Little Lemon is a charming neighborhood bistro that serves simple food
-        and classic cocktails in a lively but casual environment. We would love
-        to hear more about your experience with us!
-      </Text>
+      <ScrollView style={styles.scrollView}>
+        <Text style={styles.h1}>Welcome to Little Lemon</Text>
+        <Text style={styles.p}>
+          Little Lemon is a charming neighborhood bistro that serves simple food
+          and classic cocktails in a lively but casual environment. We would
+          love to hear more about your experience with us!
+        </Text>
+      </ScrollView>
     </View>
   );
 };
@@ -21,14 +23,19 @@ const styles = StyleSheet.create({
   h1: {
     color: "white",
     textAlign: "center",
-    fontSize: 32,
-    padding: 20,
+    fontSize: 65,
+    margin: 0,
   },
   p: {
     color: "white",
     textAlign: "center",
-    padding: 20,
-    fontSize: 30,
+    paddingTop: 100,
+    fontSize: 46,
+    lineHeight: 70,
+  },
+  scrollView: {
+    backgroundColor: "black",
+    padding: 50,
   },
 });
 
