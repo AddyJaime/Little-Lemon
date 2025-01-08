d82add07fde4a2c3c795a3587db5240cc9cccd6c
addy <addy@gmail.com>
2025-01-08 11:55:56 -0500

    still need to fixed scroll

--- src/components/WelcomeScreen.tsx
+++ src/components/WelcomeScreen.tsx
@@ -4,18 +4,17 @@ import { View, Text, StyleSheet, ScrollView } from "react-native";
 const WelcomeScreen: React.FC = () => {
   return (
     <View style={styles.container}>
-      <ScrollView indicatorStyle={"white"} style={styles.scrollView}>
+      <ScrollView
+        bounces={true}
+        indicatorStyle={"white"}
+        style={styles.scrollView}
+      >
         <Text style={styles.h1}>Welcome to Little Lemon</Text>
         <Text style={styles.p}>
           Little Lemon is a charming neighborhood bistro that serves simple food
           and classic cocktails in a lively but casual environment. We would
           love to hear more about your experience with us!
         </Text>
-        <Text style={styles.p}>
-          Here’s some additional content to enable scrolling.
-        </Text>
-        <Text style={styles.p}>We love having you here!</Text>
-        <Text style={styles.p}>Scroll down to see more.</Text>
       </ScrollView>
     </View>
   );
@@ -38,6 +37,7 @@ const styles = StyleSheet.create({
     fontSize: 38,
   },
   scrollView: {
+    flexGrow: 1,
     padding: 50,
     backgroundColor: "black",
   },
