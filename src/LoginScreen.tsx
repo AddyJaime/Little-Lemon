3af58d8074f6e80c747dfd8d5e7bd98d426acd00
addy <addy@gmail.com>
2025-01-23 22:04:26 -0500

    added login foorm but keyboard not available yet

--- src/LoginScreen.tsx
+++ src/LoginScreen.tsx
@@ -1,24 +1,71 @@
 import React, { useState } from "react";
-import { View, Text, StyleSheet, TextInput, ScrollView } from "react-native";
+import {
+  View,
+  Text,
+  StyleSheet,
+  TextInput,
+  ScrollView,
+  KeyboardAvoidingView,
+  Platform,
+} from "react-native";
 
 const LoginScreen: React.FC = () => {
   const [firstName, onChangeFirstName] = useState("");
   const [password, onChangePassoword] = useState("");
   return (
-    <ScrollView>
-      <Text style={styles.header}>Login to continue</Text>
-      <TextInput value={firstName} placeholder="Enter your first name" />
+    <KeyboardAvoidingView
+      behavior={Platform.OS === "ios" ? "padding" : "height"}
+      style={styles.container}
+    >
+      <ScrollView
+        keyboardDismissMode="on-drag"
+        keyboardShouldPersistTaps="handled"
+      >
+        <Text style={styles.header}>Login to continue</Text>
+        <TextInput
+          style={styles.nameInput}
+          value={firstName}
+          placeholder="Enter your first name"
+          onChangeText={onChangeFirstName}
+        />
 
-      <TextInput value={password} placeholder="Enter your password " />
-    </ScrollView>
+        <TextInput
+          style={styles.passwordInput}
+          value={password}
+          placeholder="Enter your password "
+          onChangeText={onChangePassoword}
+          keyboardType={"phone-pad"}
+          secureTextEntry={true}
+        />
+      </ScrollView>
+    </KeyboardAvoidingView>
   );
 };
 
 const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
   header: {
     color: "black",
     textAlign: "center",
     fontSize: 25,
+  },
+  nameInput: {
+    height: 40,
+    margin: 12,
+    borderWidth: 1,
+    padding: 10,
+    borderColor: "black",
+    backgroundColor: "white",
+  },
+  passwordInput: {
+    height: 40,
+    margin: 12,
+    borderWidth: 1,
+    padding: 10,
+    borderColor: "black",
+    backgroundColor: "white",
   },
 });
 
